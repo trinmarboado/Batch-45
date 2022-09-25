202c2fa7462578221326c155249bd0ecdda16b73
Trinmar Boado <timi.boado@gmail.com>
2022-09-25 15:40:26 +0800

    convert old UI to quasar components

--- frontend/src/pages/TodoApp.vue
+++ frontend/src/pages/TodoApp.vue
@@ -1,14 +1,33 @@
 <template>
   <div>
-    <h3 class="header pinkcolor" id="header" >todos</h3>
-    <input type="text" placeholder="What needs to be done">
-    <ul>
-      <li>asdasd</li>
-      <li>asdasd</li>
-      <li>asdasd</li>
-      <li>asdasd</li>
-      <li>asdasd</li>
-    </ul>
+    <q-toolbar class="bg-primary text-white">
+      <q-btn flat round dense icon="menu" class="q-mr-sm" />
+      <q-avatar>
+        <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+      </q-avatar>
+
+      <q-toolbar-title>Batch 45</q-toolbar-title>
+
+      <q-btn flat round dense icon="whatshot" />
+    </q-toolbar>
+    <q-input rounded outlined  modelValue="text" label="What needs to be done" />
+    <q-list bordered separator>
+      <q-item clickable v-ripple >
+        <q-item-section avatar>
+          <q-icon name="signal_wifi_off" />
+        </q-item-section>
+        <q-item-section>Active</q-item-section>
+        <q-item-section side>Side</q-item-section>
+      </q-item>
+
+      <q-item clickable v-ripple active-class="text-orange">
+        <q-item-section avatar>
+          <q-icon name="signal_wifi_off" />
+        </q-item-section>
+        <q-item-section>Active, Active class</q-item-section>
+        <q-item-section side>Side</q-item-section>
+      </q-item>
+    </q-list>
   </div>
 </template>
 
