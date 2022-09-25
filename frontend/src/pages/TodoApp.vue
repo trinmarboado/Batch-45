d6916395feff116a946ac5e52c0cf21c74821f9c
Trinmar Boado <timi.boado@gmail.com>
2022-09-25 15:33:28 +0800

    css design

--- frontend/src/pages/TodoApp.vue
+++ frontend/src/pages/TodoApp.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h3>todos</h3>
+    <h3 class="header pinkcolor" id="header" >todos</h3>
     <input type="text" placeholder="What needs to be done">
     <ul>
       <li>asdasd</li>
@@ -11,3 +11,24 @@
     </ul>
   </div>
 </template>
+
+<style>
+  h3 {
+    background-color: purple;
+    color: white;
+  }
+
+  .header {
+    margin-top: 0px;
+    margin-bottom: 0px;
+  }
+
+  .pinkcolor {
+    color: pink;
+  }
+
+  #header {
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+</style>
